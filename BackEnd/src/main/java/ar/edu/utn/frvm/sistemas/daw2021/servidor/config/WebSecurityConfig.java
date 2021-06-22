dd0dd8f2a082cb94522061a92f02b44d84f405b1
Axel Garino <axelgarino@gmail.com>
2021-06-22 20:13:26 -0300

    Actualizado al dia 22/06

--- BackEnd/src/main/java/ar/edu/utn/frvm/sistemas/daw2021/servidor/config/WebSecurityConfig.java
+++ BackEnd/src/main/java/ar/edu/utn/frvm/sistemas/daw2021/servidor/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 @Override
 protected void configure(HttpSecurity http) throws Exception {
-http.csrf().disable();
+    http.csrf().disable();
+    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    http.cors();
 }
 }
